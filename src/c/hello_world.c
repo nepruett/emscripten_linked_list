1133674c5fd0dfdca14c72541c048a322f5ac0fd
npruett <nathan.pruett@gmail.com>
2014-03-22 01:37:57 -0400

    added comment for command line process

--- src/c/hello_world.c
+++ src/c/hello_world.c
@@ -1,3 +1,9 @@
+/*
+    ./emcc
+        src/c/hello_world.c
+        -o build/js/hello_world.js
+        -s EXPORTED_FUNCTIONS="['_hello']"
+*/
 extern char *hello() {
     return "Hello World!";
 }
