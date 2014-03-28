2b0b6833b0d6301fe54823a468b3ed3ae04c2568
npruett <nathan.pruett@gmail.com>
2014-03-28 01:03:10 -0400

    added bubble sort and callback structure

---
+++ src/c/sorts.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include "linked_list.h"
+
+/*
+    ./emcc
+    ~/IdeaProjects/emscripten_linked_list/src/c/linked_list.c
+    ~/IdeaProjects/emscripten_linked_list/src/c/sorts.c
+    -o ~/IdeaProjects/emscripten_linked_list/build/js/linked_list.js
+    -s EXPORTED_FUNCTIONS="['_ll_new_node', '_ll_value', '_ll_next', '_ll_add', '_ll_remove', '_bubble_sort','_set_callback']"
+    -s RESERVED_FUNCTION_POINTERS=1
+*/
+
+void (*callback)(char *, struct Node *);
+
+void set_callback(void (*f)(char *, struct Node *)) {
+    callback = f;
+}
+
+void swap(struct Node *a, struct Node *b)
+{
+    char *temp = a->value;
+    a->value = b->value;
+    b->value = temp;
+}
+
+struct Node *bubble_sort(struct Node *list) {
+    printf("Doing BubbleSort!\n");
+    (*callback)("callback message", list);
+
+    int swapped;
+    struct Node *temp;
+    struct Node *last = NULL;
+
+    /* Checking for empty list */
+    if (list == NULL)
+        return NULL;
+
+    do {
+        swapped = 0;
+        temp = list;
+
+        while (temp->next != last)
+        {
+            if (strcmp(temp->value, temp->next->value) > 0)
+            {
+                swap(temp, temp->next);
+                swapped = 1;
+            }
+            temp = temp->next;
+        }
+        last = temp;
+    } while (swapped);
+    return list;
+}
---
+++ src/c/sorts.h
@@ -0,0 +1,8 @@
+#ifndef SORTS_H_INCLUDED
+#define SORTS_H_INCLUDED
+#include "linked_list.h"
+
+extern void set_callback(void *f);
+extern struct Node *bubble_sort(struct Node *list);
+
+#endif
