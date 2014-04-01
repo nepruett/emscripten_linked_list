59f08b0414c1d0e45ce6e9b03b3082ff50580f64
nepruett <nathan.pruett@gmail.com>
2014-04-01 14:29:01 -0400

    Update sorts.h
    Fixing callbacks to work in C w/ header

--- src/c/sorts.h
+++ src/c/sorts.h
@@ -2,7 +2,10 @@
 #define SORTS_H_INCLUDED
 #include "linked_list.h"
 
-extern void set_callback(void *f);
+typedef void (*callback_t)(const char *message, struct Node *list);
+callback_t SORT_CALLBACK;
+
+extern void set_callback(callback_t f);
 extern struct Node *bubble_sort(struct Node *list);
 
 #endif
