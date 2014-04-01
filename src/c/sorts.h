2aea72c4d838e6c43ce31008ae85cda37acbb3d4
nepruett <nathan.pruett@gmail.com>
2014-04-01 14:45:34 -0400

    Update sorts.h

--- src/c/sorts.h
+++ src/c/sorts.h
@@ -6,6 +6,7 @@ typedef void (*callback_t)(const char *message, struct Node *list);
 callback_t SORT_CALLBACK;
 
 extern void set_callback(callback_t f);
+extern struct Node *naive_bubble_sort(struct Node *list);
 extern struct Node *bubble_sort(struct Node *list);
 
 #endif
